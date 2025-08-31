b66a04ef9111c79efdb46bf8745160cb65291c57
Forud Ghafouri <fzerorubigd@gmail.com>
2025-08-31 20:09:37 +0200

    some stupid validation

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM ${BUILD_FROM}
 
 ARG BUILD_ARCH=amd64
 
+RUN apk add jq
 RUN wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-${BUILD_ARCH} -O /usr/bin/cloudflared
 RUN chmod +x /usr/bin/cloudflared
 
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
 # Create main config
-CF_TOKRN=$(bashio::config 'cf_token')
+CF_TOKEN=$(bashio::config 'cf_token')
+echo ${CF_TOKEN} | base64 -d -  2>&1 | jq &> /dev/null || echo "Make sure the token is valid (set it in configure tab)"
 /usr/bin/cloudflared tunnel --no-autoupdate run --token ${CF_TOKEN}
